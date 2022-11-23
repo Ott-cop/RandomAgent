e31bade45299abb04bd00fe55857d11713635bdf
Ottco <otavio.onoue@outlook.com>
2022-11-23 15:10:59 -0300

    Aplicação 1

---
+++ RandomAgente.py
@@ -0,0 +1,46 @@
+import random
+
+agentes = [#Controladores
+           'Astra',
+           'Omen',
+           'Brimstone',
+           'Harbor',
+           'Viper',
+           
+           #Sentinelas
+           'Sage',
+           'Cypher',
+           'Killjoy',
+           'Chamber',
+
+           #Iniciadores
+           'Breach',
+           'Sova',
+           'Fade',
+           'Skye',
+           'KAYO',
+
+           #Duelistas
+           'Jett',
+           'Raze',
+           'Reyna',
+           'Yoru',
+           'Phoenix',
+           'Neon'
+]
+
+#print(agentes[19])
+numAgentes = int(input('Número de Jogadores: '))
+
+resultado = random.shuffle(agentes)
+i = 0
+
+while (i < numAgentes):
+    
+    print('Jogador: ', agentes[i])
+    
+    i = i + 1
+
+input("")
+
+
